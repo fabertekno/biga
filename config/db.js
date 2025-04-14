55081e544dbd39b33a4c7f68b7280c1f6431f037
fabertekno <fabertekno@gmail.com>
2025-04-13 22:33:27 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         // Ensure MONGO_URI is loaded properly
-        console.log('Mongo URI:', process.env.MONGO_URI);  // Log to check the value
-        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Mongo URI:', process.env.MONGODB_URI);
+await mongoose.connect(process.env.MONGODB_URI);
+
         console.log("✅ MongoDB Connected Successfully");
     } catch (error) {
         console.error("❌ MongoDB Connection Error:", error);
