b3e84e5b96edc6dae5a9d339577515cfb223adfb
fabertekno <fabertekno@gmail.com>
2025-04-16 08:42:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: '*' })); // Allow requests from all origins
+app.use(cors({
+  origin: 'https://bigasoft.org', // Only allow this domain
+  credentials: true // Allow cookies or sessions if necessary
+}));
+
 app.use(express.json()); // Parse incoming JSON requests
 
 // Connect to MongoDB
