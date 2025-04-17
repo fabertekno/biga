5bcb7713e479e636e83f37025617edfb3af0f635
fabertekno <fabertekno@gmail.com>
2025-04-17 09:24:36 -0400

    Update invoice.js

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Invoice = require("../models/Invoice");
+const Invoice = require("../models/Invoices");
 
 // Create a new invoice
 router.post("/", async (req, res) => {
